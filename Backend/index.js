5e2fb427f67c84d35ae368a2fd99b8a3b1077195
Shivam <shivamjhax@gmail.com>
2024-05-18 04:09:16 +0530

    fixed

--- Backend/index.js
+++ Backend/index.js
@@ -13,16 +13,18 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 
+const __dirname = path.resolve();
+
 const app=express();
 app.use(express.json());
 
 app.use("/api/auth",authRoute)
 app.use("/api/notes",notesRoute)
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/Frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
 })
 
 app.listen(3000,()=>{
